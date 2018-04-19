b29559b785c98b49e5756cdf86c7099d33c8a19a
teraaret <raiiliiat@yandex.ru>
2018-04-19 13:56:05 +0500

    init. Basic functions and design done

---
+++ js/vue.index.js
@@ -0,0 +1,90 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        posts: [{
+                title: "hello",
+                text: "world"
+            }
+        ],
+        visibility: [true],
+        confirmations: true,
+        filter: ""
+    },
+    methods: {
+        condition: function (title, text, filter) {
+            if ( title.toLowerCase().indexOf(filter.toLowerCase()) != -1 )
+            { return -1; }
+            else if ( text.toLowerCase().indexOf(filter.toLowerCase()) != -1 )
+            { return -1; }
+        },
+        _add: function () {
+            this.filter = "";
+            this.posts.push({
+                title: "new post",
+                text: "text"
+            });
+            this.visibility.push(true);
+            this.save_json();
+        },
+        _edit: function (index, pos) {
+            this.visibility = new Array(this.visibility.length).fill(true);
+            this.visibility[index] = false;
+            var temp = this.visibility;
+            this.visibility = [];
+            this.visibility = temp;
+            Vue.nextTick(function () {
+                $('input').eq(pos).focus();
+            })
+        },
+        _edited: function (index) {
+            this.visibility[index] = true;
+            var temp = this.visibility;
+            this.visibility = [];
+            this.visibility = temp;
+            this.save_json();
+        },
+        _delete: function (index) {
+            if ( this.confirmations ) {
+                var question = confirm("Delete post?");
+                if (question == true) {
+                    this.posts.splice(index, 1);
+                    this.save_json();
+                }
+            } else {
+                this.posts.splice(index, 1);
+                this.save_json();
+            }
+        },
+        _confirmations: function (arg) {
+            this.confirmations = arg;
+        },
+        load_json: function () {
+            var loaded;
+            $.ajax({
+                url: "/posts/posts.json",
+                cache: false,
+                async: false,
+            }).done(function (data) {
+                loaded = data;
+            });
+            for (i = 0; i < loaded.length; i++) {
+                this.visibility.push(true);
+            }
+            return loaded;
+        },
+        save_json: function () {
+            $.ajax({
+                type: "POST",
+                url: "/app/saver.php",
+                cache: false,
+                async: false,
+                data: {
+                    data: JSON.stringify(this.posts)
+                }
+            });
+        }
+    },
+    mounted: function () {
+        this.posts = this.load_json();
+    }
+})
